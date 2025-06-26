ff69e5fb3a00a737f27dfcb918696fbb1b531b1e
David Barbosa <david.barbosa-g@mail.escuelaing.edu.co>
2025-06-25 23:41:55 -0500

    first commit

---
+++ src/main/java/com/eci/ARSW/restfullSpring/RestfullSpring/Greeting.java
@@ -0,0 +1,3 @@
+package com.eci.ARSW.restfullSpring.RestfullSpring;
+
+public record Greeting(long id, String content) { }
---
+++ src/test/java/com/eci/ARSW/restfullSpring/RestfullSpring/RestfullSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eci.ARSW.restfullSpring.RestfullSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestfullSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
